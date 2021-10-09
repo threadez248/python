0a0f9563a36ac240ddb465146af074e40ad6aa46
threadez248 <threadez248@gmail.com>
2021-10-09 08:05:17 -0400

    adding questions

---
+++ questions.py
@@ -0,0 +1,178 @@
+# # Print string
+# num1 = 4
+# num2 = 5
+
+# print(num2 < num1)
+
+# print(type(num1))
+
+# num4 = input("Enter your value: ")
+# print(num4)
+
+
+# # #USE
+# print("Hello World")
+
+# # #FIX
+# print("Python Strings")
+
+# #Predict
+aa = bb = cc = "100"
+print(bb)
+print(type(bb))
+print(bb(0))
+
+
+# #print("Life is beautiful.")
+
+# #Predict 
+# x = 5
+# y = "Five"
+# print(x)
+# print(y)
+
+#Predict 
+# x = 100
+# y = 200
+# print(x+y)
+# print(type(x+y))
+
+# #Predict 
+# x = "100"
+# y = "200"
+# print(x+y)
+# print(type(x+y))
+
+
+# # Create a variable named firstname and assign the value John to it, print the variable.
+
+# # Predict
+# x = 5
+# print(type(x)) #Choose between bool, int, str, float, list, tuple, dict
+
+# x = ["apple", "banana", "cherry"]
+# print(type(x)) #Choose between bool, int, str, float, list, tuple, dict
+
+# # Fix the code to convert x into a floating point number. 
+# x = 5
+# #Insert Line Here
+# print(type(x))
+
+# #String predict
+# x = "Hello World"
+# print(len(x))
+
+# # Extract and Print the first character of the string text.
+# txt = "Hello World"
+# #Insert code here
+
+# #Predict
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("orange")
+# print(fruits[3])
+
+# #Print the second item in the fruits list.
+# fruits = ["apple", "banana", "cherry"]
+# print()
+
+# #Use the remove method to remove "banana" from the fruits list.
+# fruits = ["apple", "banana", "cherry"]
+# #Code here
+# print(fruits)
+
+# #Insert the missing indentation to make the code correct:
+# if 5 > 2:
+# print("Five is greater than two!")
+
+# # Use negative indexing to print the last item in the tuple. 
+# fruits = ("apple", "banana", "cherry")
+# #Code here
+
+# # Use a range of indexes to print the third, fourth, and fifth item in the tuple. 
+# fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+# #Code here
+
+# #Predict
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# print(car["model"])
+
+# #Add the key/value pair "color" : "red" to the car dictionary and print
+# car =	{
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# #Predict
+# name = "John"
+# age = 23
+# if name == "John" and age == 23:
+#     print("Your name is John, and you are also 23 years old.")
+# if name == "John" or name == "Rick":
+#     print("Your name is either John or Rick.")
+    
+# #Predict
+# name = "John"
+# if name in ["John", "Rick"]:
+#     print("Your name is either John or Rick.")
+    
+
+# #Write a Python program to find if given number is divisible by 7 and multiple of 5
+# #Code here
+
+
+# # Write a Program checks if the number is positive or negative and displays an appropriate message
+# if num >= 0:
+#     print("Positive or Zero")
+# else:
+#     print("Negative number")
+    
+# #Predict
+# for x in range(0,6):
+#   print(x)
+  
+# #Predict
+# # Program to find the sum of all numbers stored in a list
+# # List of numbers
+# numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+# # variable to store the sum
+# sum = 0
+# # iterate over the list
+# for val in numbers:
+#     sum = sum+val
+
+# print(sum)
+
+# #Practice
+# # program to display student's marks from record
+# # Try changing the name to one of the names in Dictionary
+# student_name = 'Soyuj'
+# marks = {'James': 90, 'Jules': 55, 'Arthur': 77}
+# for student in marks:
+#     if student == student_name:
+#         print(marks[student])
+#         break
+# else:
+#     print('No entry with that name found.')
+    
+# # Write a FOR loop for below program that iterates the list, printing names and stops when "John" is reached.
+# names = ["Soyuj", "Jules", "Arthur", "John", "James", "Rick"]
+
+# #Predict
+# i = 1
+# while i < 4
+#   print(i)
+#   i += 1
+  
+# #In the loop, if i == 10, skip to the next iteration.
+# i = 0
+# while i < 20:
+#   i += 1
+#   #Code here
+#   #if i == 10:
+#   #  break
+#   print(i)
